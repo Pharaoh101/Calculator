65659cb811322627b4108a7631bd7c6e8b2f22ea
Aly <alysherif888@gmail.com>
2019-04-16 21:48:16 +0100

    Completed step 5
    Small issue with font family however, so keeping branch active until I can sort it

--- CalculatorSlug/App.js
+++ CalculatorSlug/App.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+// Styles
+import styles from './styles';
+
+export default class App extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.contHistory}></View>
+                <View style={styles.contOutput}>
+                    <View style={styles.placeHolderOutput}>
+                        <Text style={styles.txtDefault}>OUTPUT HERE</Text>
+                    </View>
+                </View>
+                <View style={styles.contButtons}></View>
+            </View>
+        );
+    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
