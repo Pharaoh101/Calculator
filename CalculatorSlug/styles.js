fe8ca07a2a43e0de75f013f2f35d23602f70270e
Aly <alysherif888@gmail.com>
2019-04-28 16:27:12 +0100

    Added in rest of calculator
    Major bug with rendering but can't seem to get a fix on it yet.

--- CalculatorSlug/styles.js
+++ CalculatorSlug/styles.js
@@ -3,35 +3,38 @@ const { StyleSheet } = React;
 
 export default {
 
-    container: {
-        flex: 1,
-        flexDirection: 'column'
-    },
+  container: {
+    flex:1,
+    flexDirection: 'column'
+  },
 
-    contHistory: {
-        flex: 0.3,
-        backgroundColor: 'red'
-    },
+  contHistory:{
+    flex: 0.35,
+    borderBottomWidth: 1,
+    borderColor: '#000',
+  },
 
-    contOutput: {
-        flex: 0.2
-    }, 
+  contOutput:{
+    flex: 0.25,
+  },
 
-    contButtons: {
-        flex: 0.5,
-        backgroundColor: '#bdc3c7'
-    },
+  contButtons:{
+    flex: 0.4,
+    backgroundColor: '#bdc3c7'
+  },
 
-    placeHolderOutput: {
-        flex: 1,
-        backgroundColor: 'green',
-        justifyContent: 'center',
-        alignItems: 'flex-end',
-        paddingRight: 20
-    },
+  placeHolderOutput: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+    paddingRight: 15,
+    paddingLeft: 15
+  },
 
-    txtDefault: {
-        color: '#fff',
-        fontSize: 30
-    }
+  txtDefault: {
+    color: '#000',
+    fontSize: 30
+  }
+
 };
